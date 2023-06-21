1200bd08e03020fdd8b6e753a39b638cdc1b93bf
Jianli Cheng <chengjianli90@gmail.com>
2023-06-21 13:51:04 -0700

    Add get_diffusivity and get_msd methods

--- mpmorph/util.py
+++ mpmorph/util.py
@@ -1,7 +1,7 @@
 import numpy as np
 import collections
 
-__author__ = 'Eric Sivonxay'
+__author__ = 'Jianli Cheng, Eric Sivonxay'
 
 
 def recursive_update(orig_dict, new_dict):
@@ -41,3 +41,53 @@ def msd_fft(r):
         q = q - d[m-1] - d[n-m]
         s1[m] = q / (n - m)
     return s1 - 2 * s2
+
+
+def get_diffusivity(structure, frac_coords, lattices, step_skip, time_step, t_range):
+    dt = np.arange(len(frac_coords)) * time_step * step_skip
+    if len(t_range) < 2:
+        t_range.append(dt[-1])
+    diffs = {}
+    for ele, msd in get_msd(structure, frac_coords, lattices, step_skip, time_step).items():
+        x = np.array([])
+        y = np.array([])
+        for i, v in enumerate(dt):
+            if t_range[0] < v < t_range[1]:
+                x = np.append(x, v)
+                y = np.append(y, msd[i])
+        a = np.ones((len(x), 2))
+        a[:, 0] = x
+        (m, c), _, _, _ = np.linalg.lstsq(a, y, rcond=None)
+        diffs[ele] = m / 60 / len(structure.indices_from_symbol(ele))
+    return diffs
+
+
+def get_msd(structure, frac_coords, lattices, step_skip, time_step):
+    frac_coords = np.concatenate(frac_coords, axis=1)
+    dp = frac_coords[:, 1:] - frac_coords[:, :-1]
+    dp = dp - np.round(dp)
+    f_disp = np.cumsum(dp, axis=1)
+    c_disp = []
+    for i in f_disp:
+        c_disp.append([np.dot(d, m) for d, m in zip(i, lattices[1:])])
+    c_disp = np.array(c_disp)
+    wts = [site.species.weight for site in structure]
+    dc = []
+    for i in range(len(frac_coords)):
+        frame = c_disp[:, i, :]
+        center = np.sum([v * wts[i] for i, v in enumerate(frame)], axis=0)
+        dc.append(frame - center / sum(wts))
+    dc = np.array(dc)
+    dt = np.arange(len(frac_coords)) * time_step * step_skip
+    msds = {}
+    for ele in structure.composition.elements:
+        ele = str(ele)
+        indices = structure.indices_from_symbol(ele)
+        sp_disp = dc[:, indices, :]
+        msd = np.zeros(len(dt))
+        n_atoms = len(indices)
+        for atom_num in range(n_atoms):
+            msd_temp = msd_fft(sp_disp[:, atom_num, :])
+            msd += msd_temp
+        msds[ele] = msd
+    return msds
